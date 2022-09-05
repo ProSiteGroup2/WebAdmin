db748f9ca4acde253e4f9409f2011d2d74151896
sudesh1234 <sudeshwijesinghe1@gmail.com>
2022-09-06 00:31:48 +0530

    labour icon

--- src/pages/DashboardApp.js
+++ src/pages/DashboardApp.js
@@ -109,7 +109,7 @@ export default function DashboardApp() {
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Total Labours" total={labtCount} color="error" icon={'fa-solid:tools'} />
+            <AppWidgetSummary title="Total Labours" total={labtCount} color="error" icon={'carbon:construction'} />
           </Grid>
 
          
